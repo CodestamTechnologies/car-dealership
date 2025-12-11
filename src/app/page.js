f0dbb27b3f5ada6556f9d9630527eb6bc545a0bc
_𝐵𝑜𝑛𝑑~007 <adityasingh05594@gmail.com>
2025-12-11 12:19:43 +0530

    Refactor page component and remove unused imports
    Refactor page component to remove login functionality and change background handling.

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,11 @@
-'use client'
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import Login from './login/page'
+import React from 'react'
 
-
-const Page = () => {
-  const router = useRouter()
-  const [background, setBackground] = useState("url('/car.png')");
-
-  useEffect(() => {
-    const customBackground = localStorage.getItem('login-background');
-    if (customBackground) {
-      setBackground(`url(${customBackground})`);
-    }
-  }, []);
-
-  const handleLogin = () => {
-    router.push('/dashboard')
-  }
-
+const page = () => {
   return (
-    <div className='login-background' style={{ backgroundImage: background }}>
-      <Login onLogin={handleLogin} />
+    <div>
+      Kya be Chindi
     </div>
   )
 }
 
-export default Page
-
-
+export default page
